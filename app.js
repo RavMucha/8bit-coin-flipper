ba29c3069fe46b3cb3edbcd51fa6b16f3078e27c
Rafał Mucha <r.p.mucha@gmail.com>
2021-05-13 18:30:36 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,54 @@
+document.getElementById("flipper").onclick = click;
+
+var heads = 0;
+var tails = 0;
+var head = document.getElementById("headsCount");
+var tail = document.getElementById("tailsCount");
+function click() {
+  x = Math.floor(Math.random() * 2) == 0;
+  if (x) {
+    heads++;
+    flip("heads");
+    let coin = document.createElement("i");
+    coin.classList.add("nes-icon", "coin", "is-small");
+    head.appendChild(coin);
+  } else {
+    tails++;
+    flip("tails");
+    let coin = document.createElement("i");
+    coin.classList.add("nes-icon", "coin", "is-small");
+    tail.appendChild(coin);
+  }
+  if (heads > 9) {
+    alert("heads win!");
+    heads = 0;
+    tails = 0;
+    head.innerHTML = "";
+    tail.innerHTML = "";
+    let hw = document.getElementById("HW");
+    let bigCoin = document.createElement("i");
+    bigCoin.classList.add("nes-icon", "trophy", "is-small");
+    hw.appendChild(bigCoin);
+  } else if (tails > 9) {
+    alert("tails win!");
+    heads = 0;
+    tails = 0;
+    head.innerHTML = "";
+    tail.innerHTML = "";
+    let tw = document.getElementById("TW");
+    let bigCoin = document.createElement("i");
+    bigCoin.classList.add("nes-icon", "trophy", "is-small");
+    tw.appendChild(bigCoin);
+  }
+}
+function flip(coin) {
+  let res = document.getElementById("result");
+  res.innerHTML = coin;
+  if (coin == "heads") {
+    res.classList.remove("is-error");
+    res.classList.add("is-warning");
+  } else {
+    res.classList.remove("is-warning");
+    res.classList.add("is-error");
+  }
+}
