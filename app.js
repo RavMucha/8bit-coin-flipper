94bd3e5a9673258d432384b4d4367317a976abe4
Rafał Mucha <r.p.mucha@gmail.com>
2021-05-14 11:30:50 +0200

    Added animations & links

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.getElementById("flipper").onclick = click;
-var audio = new Audio('./Assets/mario.wav');
+var audio = new Audio("./Assets/mario.wav");
 
 var heads = 0;
 var tails = 0;
@@ -9,20 +9,21 @@ function click() {
   x = Math.floor(Math.random() * 2) == 0;
   if (x) {
     heads++;
-    flip("heads");
+    flip("Heads");
     let coin = document.createElement("i");
     coin.classList.add("nes-icon", "coin", "is-small");
     head.appendChild(coin);
   } else {
     tails++;
-    flip("tails");
+    flip("Tails");
     let coin = document.createElement("i");
     coin.classList.add("nes-icon", "coin", "is-small");
     tail.appendChild(coin);
   }
   if (heads > 9) {
     document.getElementById("winner").innerHTML = "HEADS";
-    document.getElementById('dialog-dark-rounded').style.display = 'block';
+    document.getElementById("modalBg").classList.add("sectionModal");
+    document.getElementById("dialog-dark-rounded").style.display = "block";
     audio.play();
     heads = 0;
     tails = 0;
@@ -34,7 +35,8 @@ function click() {
     hw.appendChild(bigCoin);
   } else if (tails > 9) {
     document.getElementById("winner").innerHTML = "TAILS";
-    document.getElementById('dialog-dark-rounded').style.display = 'block';
+    document.getElementById("modalBg").classList.add("sectionModal");
+    document.getElementById("dialog-dark-rounded").style.display = "block";
     audio.play();
     heads = 0;
     tails = 0;
@@ -49,11 +51,16 @@ function click() {
 function flip(coin) {
   let res = document.getElementById("result");
   res.innerHTML = coin;
-  if (coin == "heads") {
+  if (coin == "Heads") {
     res.classList.remove("is-error");
     res.classList.add("is-warning");
   } else {
     res.classList.remove("is-warning");
     res.classList.add("is-error");
   }
+}
+
+function modalCloser() {
+  document.getElementById("dialog-dark-rounded").style.display = "none";
+  document.getElementById("modalBg").classList.remove("sectionModal");
 }
