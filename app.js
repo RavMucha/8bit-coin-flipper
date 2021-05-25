ca3fb1115be6f5b8eea68dd172ac8562330dea24
Rafał Mucha <r.p.mucha@gmail.com>
2021-05-25 16:37:39 +0200

    Adding PWA

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("../serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })
+}
+
 var heads = 0;
 var tails = 0;
 var headsTrophy;
---
+++ serviceWorker.js
@@ -0,0 +1,32 @@
+const staticCoinz = "retro-coin-flip"
+const assets = [
+  "/",
+  "/index.html",
+  "/style-min.css",
+  "/app-min.js",
+  "/Assets/PressStart2P-Regular.ttf",
+  "/Assets/coin.wav",
+  "/Assets/coins.png",
+  "/Assets/mario.wav",
+  "/Assets/Icons/icon-72-72.png",
+  "/Assets/Icons/icon-96-96.png",
+  "/Assets/Icons/icon-144-144.png",
+  "/Assets/Icons/icon-192-192.png",
+  "/Assets/Icons/icon-512-512.png"
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticCoinz).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+      caches.match(fetchEvent.request).then(res => {
+        return res || fetch(fetchEvent.request)
+      })
+    )
+  })
